6bdf14a36b14240cb1799280672722d58e34414d
Lục Thiên Phong <chung.bkc13061@student.bkacad.edu.vn>
2022-07-01 21:31:03 +0700

    update

---
+++ js/protect.js
@@ -0,0 +1,24 @@
+
+document.addEventListener("contextmenu", function(e){ // Prevent Right click
+    e.preventDefault();
+}, false);
+
+document.addEventListener("keydown", function (event){
+    if (event.keyCode == 123) { // Prevent F12
+        return false;
+    }
+    if (event.ctrlKey && event.shiftKey && event.keyCode == 67) { // Prevent Ctrl+Shift+C
+        return false;
+    }
+    if (event.ctrlKey && event.shiftKey && event.keyCode == 73) { // Prevent Ctrl+Shift+I
+        return false;
+    }
+    if (event.ctrlKey && event.shiftKey && event.keyCode == 74) { // Prevent Ctrl+Shift+J
+        return false;
+    }
+    if (event.ctrlKey && (event.keyCode === 85 || event.keyCode === 117)) { // Prevent Ctrl+U
+        return false;
+    }   else {
+        return true;
+    }
+});
